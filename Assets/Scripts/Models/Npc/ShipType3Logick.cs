0037fd340ef877c357be5442ccb05fa16d887eaa
LittiSecond <breeze2882@yandex.ru>
2023-03-06 08:23:13 +0300

    ShipType3 Movement logick

--- Assets/Scripts/Models/Npc/ShipType3Logick.cs
+++ Assets/Scripts/Models/Npc/ShipType3Logick.cs
@@ -2,6 +2,23 @@
 {
     public sealed class ShipType3Logick : NpcBaseLogick
     {
+        #region Fields
+
+        private ShipType3Movement _movement;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _movement = new ShipType3Movement(transform, _rigidbody);
+            AddExecutable(_movement);
+        }
+
+        #endregion
 
     }
 }
--- Assets/Scripts/Models/Npc/ShipType3Movement.cs
+++ Assets/Scripts/Models/Npc/ShipType3Movement.cs
@@ -10,15 +10,15 @@ namespace Dragoraptor
 
         private readonly Transform _transform;
         private readonly Rigidbody2D _rigidbody;
+
+        private Vector3 _velocity;
+
         private float _horizontalSpeed = 0.5f;
         private float _verticalSpeed = 0.1f;
         private float _minY = -1.0f;
         private float _maxXError = 0.1f;
 
-        #endregion
-
-
-        #region Properties
+        private Direction _currentDirection;
 
         #endregion
 
@@ -36,6 +36,30 @@ namespace Dragoraptor
 
         #region Methods
 
+        private float CalculateVerticalSpeed()
+        {
+            return (_transform.position.y > _minY) ? -_verticalSpeed : 0.0f; 
+        }
+
+        private float CalculateHorizontalSpeed(float targetX)
+        {
+            float speed = 0.0f;
+            float x = _transform.position.x;
+
+            float dx = targetX - x;
+
+            if (dx > _maxXError)
+            {
+                speed = _horizontalSpeed;
+            }
+            else if (dx < -_maxXError)
+            {
+                speed = -_horizontalSpeed;
+            }
+
+            return speed;
+        }
+
         #endregion
 
 
@@ -43,14 +67,18 @@ namespace Dragoraptor
 
         public void Execute()
         {
+            Vector3 newVelocity = Vector3.zero;
             Vector3? playerPosition = Services.Instance.CharacterIntermediary.GetPlayerPosition();
             if (playerPosition.HasValue)
             {
-
+                newVelocity.x = CalculateHorizontalSpeed(playerPosition.Value.x);
+                newVelocity.y = CalculateVerticalSpeed();
             }
-            else
-            {
 
+            if (newVelocity != _velocity)
+            {
+                _rigidbody.velocity = newVelocity;
+                _velocity = newVelocity;
             }
 
         }
